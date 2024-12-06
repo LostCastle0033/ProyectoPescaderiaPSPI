5e1d204bbb64616473365c03611d689aa22876d7
DamianLZ <157612708+DamianLZ@users.noreply.github.com>
2024-12-06 16:22:14 -0600

    Add files via upload

---
+++ pescaderiapsip/PescaderiaPSIP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pescaderiapsip;
+
+/**
+ *
+ * @author pablo
+ */
+public class PescaderiaPSIP {
+
+    public static void main(String[] args) {
+        Pt1 Pt1 = new Pt1();
+        Pt1.setVisible(true);
+    }
+}
